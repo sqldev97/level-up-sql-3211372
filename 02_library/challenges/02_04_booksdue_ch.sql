b2dfca9f5e086fdde7a72814a5c78ae89a19a104
sqldev97 <153027251+sqldev97@users.noreply.github.com>
2023-12-06 22:23:41 +0000

    library 2

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,9 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+
+select L.DueDate , b.Title, P.FirstName, P.LastName, P.Email
+from loans L
+  inner join books b on l.BookID = b.BookID
+  inner join Patrons p on l.PatronID = p.PatronID
+where L.duedate = '2022-07-13' and L.returneddate is null
--- 02_library/challenges/02_05_returnbooks_ch.sql
+++ 02_library/challenges/02_05_returnbooks_ch.sql
@@ -4,3 +4,9 @@
 -- Book 1: 6435968624
 -- Book 2: 5677520613
 -- Book 3: 8730298424
+
+select * from books where barcode in (6435968624, 5677520613, 8730298424)
+
+select * from loans where ReturnedDate = '2022-07-05' and bookid in (46, 73, 105)
+
+update loans set ReturnedDate = '2022-07-05' where ReturnedDate is null and bookid in (select BookID from books where barcode in (6435968624, 5677520613, 8730298424))
